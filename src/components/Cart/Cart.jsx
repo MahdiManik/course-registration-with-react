8321d81ed3a50c7211e62474f16b701ed7057979
Coder Mahdi <codermahdihasan@gmail.com>
2023-09-15 10:16:33 +0600

    clicking button to display the course's name in the shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,39 @@
 
 import PropTypes from 'prop-types'
 
-const Cart = cart => {
+const Cart = ({ selectedCourse }) => {
   return (
-    <div>
-      <h3 className='text-3xl font-semibold'>Cart: {courses.length}</h3>
-    </div>
+    <>
+      <div className='bg-white p-4 rounded-lg'>
+        <h3 className='text-3xl font-semibold pb-4'>Cart: {selectedCourse.length}</h3>
+        <hr />
+        <h3 className='font-semibold text-lg mb-5 pl-2'>Course Name</h3>
+        {
+          selectedCourse.map((course, index) => (
+
+            <>
+
+              <div key={course.id} className='py-1 pl-2'>
+
+                <ol>
+                  <li className="text-md">{index + 1} {course.title}</li>
+                </ol>
+                <hr />
+              </div>
+
+            </>
+          ))
+        }
+        <div className=''>
+          <h4>Total Credit Hour : {selectedCourse.credit}</h4>
+        </div>
+      </div>
+    </>
   )
 }
 
 Cart.propTypes = {
-  cart.PropTypes.object
+  selectedCourse: PropTypes.array.isRequired
 }
 
 export default Cart
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,8 +5,10 @@ import Cart from '../Cart/Cart';
 
 const Home = () => {
 
-
   const [courses, setCourses] = useState([]);
+
+  const [selectedCourse, setSelectedCourse] = useState([]);
+
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
@@ -14,16 +16,19 @@ const Home = () => {
   }, [])
 
 
+  const handleSelectCourse = course => {
+    setSelectedCourse([...selectedCourse, course]);
+  }
 
   return (
 
     <div className='bg-gray-200'>
       <h3 className='text-3xl font-semibold text-center pt-10'>Course Registration: {courses.length}</h3>
-      <div className="flex items-start justify-center mt-4 gap-6">
+      <div className="flex items-start justify-center mt-4 gap-4">
         <div className='w-4/6'>
           <div className='grid grid-cols-3 gap-6'>
             {courses.map(course => (
-              <div key={course.title} className=' bg-white gap-3  space-y-2  flex flex-col justify-start p-4 items-center  rounded-md mt-12'>
+              <div key={course.id} className=' bg-white gap-3  space-y-2  flex flex-col justify-start p-4 items-center  rounded-lg mt-12'>
                 <div className=''>
                   <img className=' rounded-md' src={course.image} alt="" />
                 </div>
@@ -33,27 +38,29 @@ const Home = () => {
                   <p>Price: ${course.course_price}</p>
                   <p>Credit: {course.credit}</p>
                 </div>
-                
-                <button className='px-6 py-2 text-white bg-blue-600 w-full rounded-lg'>Select</button>
+
+                <button onClick={() => handleSelectCourse(course)} className='px-6 py-2 text-white bg-blue-600 w-full rounded-lg'>Select</button>
               </div>
             ))
             }
           </div>
 
-      </div>
-          <div className='w-1/4 mt-8'>
-          {/* {courses.map(course => (
-
-            <Cart key={course.title} cart={cart} ></Cart>
-          ))} */}
-            
-          </div>
         </div>
+        <div className='w-1/4 mt-12'>
+
+            <Cart 
+            key={courses.title}
+            selectedCourse={selectedCourse}
+            ></Cart>
+  
+
+        </div>
+      </div>
 
     </div>
 
-  )
-}
+  );
+};
 
 
 Home.propTypes = {
